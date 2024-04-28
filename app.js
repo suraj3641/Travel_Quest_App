2c0f47be28d79e7ab219d29e6d2149189bb2c97d
suraj3641 <mauryasuraj364@gmail.com>
2024-04-28 16:14:51 +0530

    first commit

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const userRouter=require("./routes/user.js");
 const { date } = require("joi");
 
 //connection of DB
-// const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+// const MONGO_URL="mongodb://127.0.0.1:27017/TravelQuest";
 
 const dbUrl= process.env.ATLASDB_URL;
 
